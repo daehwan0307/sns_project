da431f435827238a7791d11f786abe14ef89fe34
대환 <eoghks960307@gmail.com>
2022-12-26 09:04:29 +0900

    keyword change

--- src/main/java/com/example/sns_project/controller/HelloController.java
+++ src/main/java/com/example/sns_project/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello(){
-        return ResponseEntity.ok().body("popin");
+        return ResponseEntity.ok().body("happy_new_year");
     }
 }
