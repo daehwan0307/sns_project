9ccaf57b1372257b2f65198bd6999b5ef0b09682
대환 <eoghks960307@gmail.com>
2022-12-26 11:26:29 +0900

    token 발행

--- src/main/java/com/example/sns_project/service/UserService.java
+++ src/main/java/com/example/sns_project/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.sns_project.domain.entity.User;
 import com.example.sns_project.exception.AppException;
 import com.example.sns_project.exception.ErrorCode;
 import com.example.sns_project.repository.UserRepository;
+import com.example.sns_project.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,10 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String key;
+    private Long expireTimeMs = 1000*60*60l;
 
     public String join(String userName, String password){
         //userName 중복  check
@@ -40,13 +45,15 @@ public class UserService {
         User selectedUser = userRepository.findByUserName(userName).orElseThrow(()->new AppException(ErrorCode.USERNAME_NOT_FOUND,userName+"이 없습니다."));
 
         //password 틀림
-        if(!encoder.matches(selectedUser.getPassword(),password)){
+        if(!encoder.matches(password,selectedUser.getPassword())){
             throw new AppException(ErrorCode.INVALID_PASSWORD,"패스워드를 잘못 입력 했습니다.");
 
         }
 
+        String token  = JwtTokenUtil.createToken(selectedUser.getUserName(),key,expireTimeMs);
+
         //
 
-        return "token";
+        return token;
     }
 }
---
+++ src/main/java/com/example/sns_project/utils/JwtTokenUtil.java
@@ -0,0 +1,24 @@
+package com.example.sns_project.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+public class JwtTokenUtil {
+
+    //token 사용123
+    public static String createToken(String userName,String key,long expireTimeMs){
+        Claims claims = Jwts.claims();
+        claims.put("userName",userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expireTimeMs))
+                .signWith(SignatureAlgorithm.HS256,key)
+                .compact()
+                ;
+    }
+}
+
