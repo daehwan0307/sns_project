bb58a2066b0efb0ac6a38b4bfd7d907e4010ed18
대환 <eoghks960307@gmail.com>
2023-01-04 14:14:51 +0900

    [Add] Comment Entity

---
+++ src/main/java/com/example/sns_project/domain/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.example.sns_project.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Comment extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+
+}
