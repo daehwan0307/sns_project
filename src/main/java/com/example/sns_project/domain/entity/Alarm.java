60ba24fea18b4456fcfccf31ce5d3b5f787a634e
대환 <eoghks960307@gmail.com>
2023-02-25 22:38:54 +0900

    [Feat] Alarm Entity 생성 (초기)

--- src/main/java/com/example/sns_project/domain/entity/Alarm.java
+++ src/main/java/com/example/sns_project/domain/entity/Alarm.java
@@ -1,4 +1,37 @@
 package com.example.sns_project.domain.entity;
 
-public class Alarm {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Builder
+@Entity
+public class Alarm  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String fromUserName;
+    private String massage;
+    private boolean readOrNot;
+
+    public boolean getReadOrNot() {
+
+        return readOrNot;
+    }
+    public void setReadOrNot() {
+
+        this.readOrNot = true;
+    }
 }
+
